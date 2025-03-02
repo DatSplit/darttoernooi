3dd07b45450a4beb6f812105fca986e8e0277e48
DatSplit <nielsteunissen@hotmail.nl>
2025-03-02 16:31:31 +0100

    updated requirements text and data saving.

--- streamlit-web-app/src/streamlit_app.py
+++ streamlit-web-app/src/streamlit_app.py
@@ -5,31 +5,48 @@ import streamlit.components.v1 as components
 def display_image(file_path):
     st.image(file_path, caption="QR-code voor betaling, €5 per team")
 
-def save_data(name, tournament_type, entrance_fee):
-    with open("submissions.txt", "a", encoding="utf-8") as f:
-        f.write(f"{name},{tournament_type},{entrance_fee}\n")
+
+def add_submission_to_sharepoint(name, tournament_type):
+    from office365.sharepoint.client_context import ClientContext
+    from office365.runtime.auth.user_credential import UserCredential
+
+    site_url = st.secrets["sharepoint"]["site_url"]
+    username = st.secrets["sharepoint"]["username"]
+    password = st.secrets["sharepoint"]["password"]
+    list_name = st.secrets["sharepoint"]["list_name"]
+
+    ctx = ClientContext(site_url).with_credentials(UserCredential(username, password))
+    sp_list = ctx.web.lists.get_by_title(list_name)
+    item_properties = {
+        "Title": "aanmelding",
+        "Name": name,
+        "Toernooi_type": tournament_type
+    }
+    sp_list.add_item(item_properties)
+    ctx.execute_query()
     return True
 
 def main():
     st.title("Dart-toernooi VVA Achterberg 11 april 2025 aanmeldformulier")
     st.text("Welkom bij het aanmeldformulier voor het dart-toernooi van VVA Achterberg op 11 april 2025.")
     st.text("Het toernooi gaat plaatsvinden op vrijdag 11 april 2025 van ... tot ... . U kunt zich aanmelden voor het singlestoernooi en het dubbels toernooi. Kosten voor deelnames zijn ... Vul onderstaande gegevens in om je aan te melden voor het toernooi.")
+    
     # User input for name
     name = st.text_input("Voer je naam in:")
     
     # Tournament type selection
     tournament_type = st.selectbox("Kies het type toernooi:", ["Singles", "Doubles"])
 
-    # Entrance fee (for display and saving)
-    entrance_fee = 5
 
     st.text("QR-code voor betaling, €5 euro per team")
     display_image("streamlit-web-app/src/betalen.png")
     
     if st.button("Verzend aanmelding"):
-        if name and tournament_type:
-            save_data(name, tournament_type, entrance_fee)
-            st.success("Aanmelding succesvol verzonden!")
+            try:
+                add_submission_to_sharepoint(name, tournament_type)
+                st.success("Aanmelding succesvol verzonden!")
+            except Exception as e:
+                st.error(f"Aanmelding opgeslagen maar mislukt om naar SharePoint te uploaden: {e}")
         else:
             st.error("Vul alstublieft alle velden in.")
 
