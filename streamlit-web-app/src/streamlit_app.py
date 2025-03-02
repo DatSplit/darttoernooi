be37a0d66d6a117b9df5db5ddbdd8b05b4b9c913
DatSplit <nielsteunissen@hotmail.nl>
2025-03-02 16:12:18 +0100

    html component.

--- streamlit-web-app/src/streamlit_app.py
+++ streamlit-web-app/src/streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import base64
+import streamlit.components.v1 as components
 
 def display_pdf(file_path):
     with open(file_path, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="900" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+    components.html(pdf_display, width=700, height=900)
 
 def save_to_database(name, tournament_type, entrance_fee):
     with open("submissions.txt", "a", encoding="utf-8") as f:
